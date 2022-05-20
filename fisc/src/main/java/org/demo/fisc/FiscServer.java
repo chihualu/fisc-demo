e744c9012794d38a379a9ec846f96dda5466e17f
A0620 <111111>
2022-05-20 15:57:33 +0800

    test fisc server

--- fisc/src/main/java/org/demo/fisc/FiscServer.java
+++ fisc/src/main/java/org/demo/fisc/FiscServer.java
@@ -4,11 +4,15 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.log4j.Log4j2;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 @Log4j2
 public class FiscServer {
-
+    private static Set<String> stans = new HashSet<>();
     static ObjectMapper objectMapper = new ObjectMapper();
 
     public String sendToFiscAndGetRsp(String reqJson) {
@@ -18,23 +22,51 @@ public class FiscServer {
 
             Map<String, String> rspMap = objectMapper.readValue(reqJson, Map.class);
 
-            for (Map.Entry<String, String> entry : map.entrySet()) {
-                log.debug("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+//            for (Map.Entry<String, String> entry : map.entrySet()) {
+//                log.debug("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+//            }
+
+            rspMap.put("txnType", "0210");
+            rspMap.put("txnCode", map.get("txnCode"));
+            rspMap.put("txnDateTime", map.get("txnDateTime"));
+            rspMap.put("txnStan", map.get("txnStan"));
+            String date = new SimpleDateFormat("yyyyMMdd").format(new Date()) ;
+            if(map.get("returnCode") == null ||  !"0000".equals(map.get("returnCode") )) {
+                rspMap.put("returnCode", "2999");
+            }
+            if(map.get("txnDateTime") == null || !map.get("txnDateTime").substring(0, 8).equals(date) ) {
+                rspMap.put("returnCode", "2999");
+            }
+            try{
+                if(map.get("txnStan") == null ||  Integer.parseInt(map.get("txnStan")) < 0 ) {
+                    rspMap.put("returnCode", "2999");
+                }
+                if(map.get("txnStan") != null) {
+                    if(stans.contains(map.get("txnStan"))) {
+                        rspMap.put("returnCode", "2999");
+                    }else{
+                        stans.add(map.get("txnStan"));
+                    }
+                }
+            }catch (Exception e){
+                rspMap.put("returnCode", "2999");
             }
 
-            if (map.get("txnCode").equals("3100")) {
-                rspMap.put("txnType", "0210");
-                rspMap.put("returnCode", "0000");
-            }
 
-            if (map.get("txnCode").equals("3109")) {
-                rspMap.put("txnType", "0210");
-                p3109process(rspMap);
+            if("0000".equals(rspMap.get("returnCode"))) {
+                if (map.get("txnCode").equals("3100")) {
+                    rspMap.put("returnCode", "0001");
+                }else if (map.get("txnCode").equals("3201") || map.get("txnCode").equals("3109")) {
+                    p3109process(rspMap);
+                }else {
+                    rspMap.put("returnCode", "2999");
+                }
+
             }
 
             rspStr = objectMapper.writeValueAsString(rspMap);
         } catch (JsonProcessingException e) {
-            e.printStackTrace();
+            log.error("", e);
         }
 
         return rspStr;
@@ -48,31 +80,27 @@ public class FiscServer {
         }
 
         String bankId = rspMap.get("bankCode");
-
+        rspMap.put("returnCode", "0001");
         switch (bankId){
-            case "987":
-                rspMap.put("returnCode", "0000");
+            case "983":
                 rspMap.put("fiscStatus","1");
+                rspMap.put("bankStatus","3");
+                rspMap.put("appStatus","1");
+                break;
+
+            case "982":
+                rspMap.put("fiscStatus","3");
                 rspMap.put("bankStatus","1");
                 rspMap.put("appStatus","1");
                 break;
 
-            case "809":
-                rspMap.put("returnCode", "0000");
+            case "981":
                 rspMap.put("fiscStatus","1");
                 rspMap.put("bankStatus","1");
-                rspMap.put("appStatus","1");
+                rspMap.put("appStatus","3");
                 break;
 
-            case "012":
-                rspMap.put("returnCode", "0000");
-                rspMap.put("fiscStatus","1");
-                rspMap.put("bankStatus","1");
-                rspMap.put("appStatus","1");
-                break;
-
-            case "013":
-                rspMap.put("returnCode", "0000");
+            case "700":
                 rspMap.put("fiscStatus","1");
                 rspMap.put("bankStatus","1");
                 rspMap.put("appStatus","1");
